a0049ca35431045cc42b48e51ee686efa91d552c
Akbar Attamimi <makbar703@gmail.com>
2023-10-18 09:59:59 +0000

    Edit script

--- etl_web_2_gcs.py
+++ etl_web_2_gcs.py
@@ -17,8 +17,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
 
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    df.iloc[:, 1] = pd.to_datetime(df.iloc[:, 1])
+    df.iloc[:, 2] = pd.to_datetime(df.iloc[:, 2])
 
     print(df.head(2))
     print(df.dtypes)
